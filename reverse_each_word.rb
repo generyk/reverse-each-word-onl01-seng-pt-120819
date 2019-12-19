8385a7aa2e426f3d01259e6d2d902425947c1e25
Eryk Rachwal <erykrachwal@yahoo.com>
2019-12-19 04:49:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,5 +3,6 @@ def reverse_each_word(sentence)
  split_sentence_array = sentence.split 
  reversed_array = split_sentence_array.collect do |word|
    word.reverse 
+  reversed_array.join(" ")
   end 
 end
