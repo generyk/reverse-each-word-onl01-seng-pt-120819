eb268702488ed65676846783b39901627ad9026b
Eryk Rachwal <erykrachwal@yahoo.com>
2019-12-19 04:43:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
-def reverse_each_word(sentence1)
-  string.split(" ").each do |word|
-  
-  new_sentence << word.reverse 
-  end
-  new_sentence
+def reverse_each_word(sentence)
+ reversed_sentence= [ ]
+ split_sentence_array = sentence.split 
+ split_sentence_array.map do |word|
+   word.reverse 
+ end 
 end
